f4d69481a57b3999424d552e5624bb5523c458d8
nori <74483073+nori125@users.noreply.github.com>
2021-02-20 18:07:52 +0900

    sample commit

--- script.js
+++ script.js
@@ -1,39 +1,83 @@
-const Peer = require('skyway-js');
-const peer = new Peer({key: '6c484e4b-71d6-40a9-9db2-b94d143bdfaf'});
+const Peer = window.Peer;
 
-peer = new Peer({
-    key: '6c484e4b-71d6-40a9-9db2-b94d143bdfaf',
-    debug: 3
-});
+(async function main() {
+  const localVideo = document.getElementById('js-local-stream');
+  const localId = document.getElementById('js-local-id');
+  const callTrigger = document.getElementById('js-call-trigger');
+  const closeTrigger = document.getElementById('js-close-trigger');
+  const remoteVideo = document.getElementById('js-remote-stream');
+  const remoteId = document.getElementById('js-remote-id');
+  const meta = document.getElementById('js-meta');
+  const sdkSrc = document.querySelector('script[src*=skyway]');
 
-peer.on('open', () => {
-    document.getElementById('my-id').textContent = peer.id;
-});
+  meta.innerText = `
+    UA: ${navigator.userAgent}
+    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
+  `.trim();
 
-document.getElementById('make-call').onclick = () => {
-    const theirID = document.getElementById('their-id').value;
-    const mediaConnection = peer.call(theirID, localStream);
-    setEventListener(mediaConnection);
-  };
+  const localStream = await navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true,
+    })
+    .catch(console.error);
 
-  const setEventListener = mediaConnection => {
-    mediaConnection.on('stream', stream => {
-      // Set a camera image to the video element and play it
-      const videoElm = document.getElementById('their-video')
-      videoElm.srcObject = stream;
-      videoElm.play();
+  // Render local stream
+  localVideo.muted = true;
+  localVideo.srcObject = localStream;
+  localVideo.playsInline = true;
+  await localVideo.play().catch(console.error);
+
+  const peer = (window.peer = new Peer({
+    key: window.__SKYWAY_KEY__,
+    debug: 3,
+  }));
+
+  // Register caller handler
+  callTrigger.addEventListener('click', () => {
+    // Note that you need to ensure the peer has connected to signaling server
+    // before using methods of peer instance.
+    if (!peer.open) {
+      return;
+    }
+
+    const mediaConnection = peer.call(remoteId.value, localStream);
+
+    mediaConnection.on('stream', async stream => {
+      // Render remote stream for caller
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
     });
-  }
 
+    mediaConnection.once('close', () => {
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.srcObject = null;
+    });
+
+    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
+  });
+
+  peer.once('open', id => (localId.textContent = id));
+
+  // Register callee handler
   peer.on('call', mediaConnection => {
     mediaConnection.answer(localStream);
-    setEventListener(mediaConnection);
+
+    mediaConnection.on('stream', async stream => {
+      // Render remote stream for callee
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
+    });
+
+    mediaConnection.once('close', () => {
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.srcObject = null;
+    });
+
+    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
   });
 
-  peer.on('error', err => {
-    alert(err.message);
-  });
-
-  peer.on('close', () => {
-    alert('We have lost communication.');
-  });
+  peer.on('error', console.error);
+})();
