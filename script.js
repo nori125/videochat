8bbce02fd189a156913fbd761649b35d2396256e
nori <74483073+nori125@users.noreply.github.com>
2021-02-20 17:50:14 +0900

    use_defultcode

--- script.js
+++ script.js
@@ -1,89 +1,41 @@
 'use strict';
 
-let localStream = null;
-let peer = null;
-let existingCall = null;
-
 const Peer = require('skyway-js');
 const peer = new Peer({key: '6c484e4b-71d6-40a9-9db2-b94d143bdfaf'});
-
-navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then(function (stream) {
-        // Success
-        $('#my-video').get(0).srcObject = stream;
-        localStream = stream;
-    }).catch(function (error) {
-    // Error
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
-});
 
 peer = new Peer({
     key: '6c484e4b-71d6-40a9-9db2-b94d143bdfaf',
     debug: 3
 });
 
-peer.on('open', function(){
-    $('#my-id').text(peer.id);
+peer.on('open', () => {
+    document.getElementById('my-id').textContent = peer.id;
 });
 
-peer.on('error', function(err){
+document.getElementById('make-call').onclick = () => {
+    const theirID = document.getElementById('their-id').value;
+    const mediaConnection = peer.call(theirID, localStream);
+    setEventListener(mediaConnection);
+  };
+
+  const setEventListener = mediaConnection => {
+    mediaConnection.on('stream', stream => {
+      // Set a camera image to the video element and play it
+      const videoElm = document.getElementById('their-video')
+      videoElm.srcObject = stream;
+      videoElm.play();
+    });
+  }
+
+  peer.on('call', mediaConnection => {
+    mediaConnection.answer(localStream);
+    setEventListener(mediaConnection);
+  });
+
+  peer.on('error', err => {
     alert(err.message);
-});
+  });
 
-peer.on('close', function(){
-});
-
-peer.on('disconnected', function(){
-});
-
-$('#make-call').submit(function(e){
-    e.preventDefault();
-    const call = peer.call($('#callto-id').val(), localStream);
-    setupCallEventHandlers(call);
-});
-
-$('#end-call').click(function(){
-    existingCall.close();
-});
-
-peer.on('call', function(call){
-    call.answer(localStream);
-    setupCallEventHandlers(call);
-});
-
-function setupCallEventHandlers(call){
-    if (existingCall) {
-        existingCall.close();
-    };
-
-    existingCall = call;
-
-    call.on('stream', function(stream){
-        addVideo(call,stream);
-        setupEndCallUI();
-        $('#their-id').text(call.remoteId);
-    });
-    call.on('close', function(){
-        removeVideo(call.remoteId);
-        setupMakeCallUI();
-    });
-}
-
-function addVideo(call,stream){
-    $('#their-video').get(0).srcObject = stream;
-}
-
-function removeVideo(peerId){
-    $('#'+peerId).remove();
-}
-
-function setupMakeCallUI(){
-    $('#make-call').show();
-    $('#end-call').hide();
-}
-
-function setupEndCallUI() {
-    $('#make-call').hide();
-    $('#end-call').show();
-}
+  peer.on('close', () => {
+    alert('We have lost communication.');
+  });
